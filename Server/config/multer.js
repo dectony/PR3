df83514e3e81575ea117d47a9635455fa39c2b93
Anton Zasenka <anton.zasenka@icx.local>
2016-05-19 11:43:27 +0300

    ng-file-upload added

--- Server/config/multer.js
+++ Server/config/multer.js
@@ -2,7 +2,7 @@ var multer = require('multer');
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, '/tmp/my-uploads')
+        cb(null, __dirname + '/../../Uploads')
     },
     filename: function (req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now())
